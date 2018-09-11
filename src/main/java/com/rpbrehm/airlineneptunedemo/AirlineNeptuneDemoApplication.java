ec741ff69e9c14cc10f617807d505d69d36ca0f0
rpbrehm <rpbrehm@gmail.com>
2018-09-11 08:02:45 -0700

    Initial commit

---
+++ src/main/java/com/rpbrehm/airlineneptunedemo/AirlineNeptuneDemoApplication.java
@@ -0,0 +1,12 @@
+package com.rpbrehm.airlineneptunedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineNeptuneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineNeptuneDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rpbrehm/airlineneptunedemo/AirlineNeptuneDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rpbrehm.airlineneptunedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirlineNeptuneDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
